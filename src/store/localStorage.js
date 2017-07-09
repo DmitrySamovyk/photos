f2152fb922be085c3125967bb5fab750b46ac7af
DmitriySamovyk <samovyk.d@gmail.com>
2017-07-09 23:54:42 +0300

    fix state name

--- src/store/localStorage.js
+++ src/store/localStorage.js
@@ -13,7 +13,7 @@ export const loadState = () => {
 export const saveState = (state) => {
   try {
     const serializedState = JSON.stringify({state, timestamp: new Date().getTime()});
-    localStorage.setItem('Tab42State', serializedState);
+    localStorage.setItem('AppState', serializedState);
   } catch (err) {
     // Ignore errors
   }
